23c7a4d287a87235198d40dcc5994f1a173a7aa5
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-03 17:12:24 +0300

    Add the comment

--- builtins.js
+++ builtins.js
@@ -5,4 +5,8 @@ console.log(obj.__proto__);
 const str = "str";
 
 console.log(str.__proto__);
+
+// while executing
+// str.toUpperCase(), it will call
+// new String()
 console.log(str.toUpperCase());
